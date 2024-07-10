79ec93fd1757ff29bc796da97d69015e30d44622
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2024-07-10 09:25:52 +0900

    🏷️  add types

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,4 +1,15 @@
+export type APIError = {
+  ErrorCode: number;
+  ErrorMessageJP: string;
+  ErrorMessageEN: string;
+};
+
 export type ThreadInfo = {
+  id: string;
+  title: string;
+};
+
+export type CreateThreadResponse = {
   id: string;
   title: string;
 };
